7da274fba879220b484001856796a7cde414dc72
HT <hoangthanh@gxd.vn>
2021-06-17 01:36:11 -0700

    lai

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -32,11 +32,11 @@ class AuthServiceProvider extends ServiceProvider
         // Passport::refreshTokensExpireIn(Carbon::now()->addDays(30));
         // Passport::pruneRevokedTokens();// xoa token het han
 
-        
-        // Permission::get()->map(function($permission){
-        //     Gate::define($permission->slug, function($user) use ($permission){
-        //        return $user->hasPermission($permission);
-        //     });
-        // });
+
+        Permission::get()->map(function($permission){
+            Gate::define($permission->slug, function($user) use ($permission){
+               return $user->hasPermission($permission);
+            });
+        });
     }
 }
