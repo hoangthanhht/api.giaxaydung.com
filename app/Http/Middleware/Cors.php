c8d7ba9551c2cc7fce2c7c6d72a01739d7632d7c
HT <hoangthanh@gxd.vn>
2021-06-30 00:49:40 -0700

    cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -3,22 +3,36 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\Request;
 
 class Cors
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle(Request $request, Closure $next)
+  /**
+   * Handle an incoming request.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \Closure  $next
+   * @return mixed
+   */
+    public function handle($request, Closure $next)
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization');
+        $headers = [
+            'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Headers' => 'Content-Type, Authorization, Origin, Text-X',
+            'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
+            'Access-Control-Expose-Headers' => 'Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Credentials ',
+            'GXD-Version' => '1.0'
+        ];
+
+        if ( $request->getMethod() === 'OPTIONS' ) {
+            return response()
+                    ->json(['status' => 'success'])
+                    ->withHeaders($headers);
+        }
+
+        $response = $next($request);
+
+        $response->headers->add( $headers );
+
+        return $response;
     }
 }
