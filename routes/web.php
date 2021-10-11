a5772ebdc04b768542bf4e792cd2c7e364b516a3
HT <hoangthanh@gxd.vn>
2021-10-11 08:14:16 +0700

    sua lai view

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/dl', function () {
-    return response()->download(public_path() . '/storage/bk/' . 'file1.txt');
+Route::get('/', function () {
+    return view('welcome');
 });
